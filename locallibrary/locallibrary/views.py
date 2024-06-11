c2c199bef965ede419aa8dc29c226f20b0b84716
Nabin Prasad Dhungana <prnabin123@gmail.com>
2024-06-11 09:01:56 +0545

    Subject for email managed

--- locallibrary/locallibrary/views.py
+++ locallibrary/locallibrary/views.py
@@ -2,6 +2,9 @@ from .forms import CustomPasswordResetForm
 from django.contrib.auth.views import PasswordResetView
 from django.urls import reverse_lazy
 
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse
+
 class CustomPasswordResetView(PasswordResetView):
     form_class = CustomPasswordResetForm
     template_name = 'registration/password_reset_form.html'
@@ -9,3 +12,15 @@ class CustomPasswordResetView(PasswordResetView):
     subject_template_name = 'registration/password_reset_subject.txt'
     success_url = reverse_lazy('password_reset_done')
 
+    def form_valid(self, form):
+        try:
+            form.save(
+                use_https=self.request.is_secure(),
+                from_email='webmaster@localhost',
+                request=self.request,
+            )
+            return super().form_valid(form)
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        except Exception as e:
+            return HttpResponse(f'An error occurred: {e}')
